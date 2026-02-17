a305e95d15d7df15529a63904eee8c59c034dcb6
Adicionales Deployer <deploy@adicionales.app>
2026-02-17 03:26:24 -0300

    Fix sw.js to point to app.js

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-const CACHE_NAME = 'adicionales-sf-v3';
+const CACHE_NAME = 'adicionales-sf-v4';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
-    './index.html',
-    './app-bundle.js',
-    './styles.css',
+    './app.js',
     './styles.css',
     'https://cdn.tailwindcss.com?plugins=forms,container-queries',
     'https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700;800&family=Inter:wght@300;400;500;600;700&display=swap',
