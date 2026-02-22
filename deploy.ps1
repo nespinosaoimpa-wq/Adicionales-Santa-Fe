f08a110150d11d049aa9c9b415a09e38895243c6
Adicionales Deployer <deploy@adicionales.app>
2026-02-22 13:37:51 -0300

    Deploying v1.9.2 - Hard Cache Reset & SW v10

--- deploy.ps1
+++ deploy.ps1
@@ -12,7 +12,7 @@ if (-not (Test-Path ".git")) {
 }
 
 git add .
-git commit -m "Deploying v1.9.0 - Premium Admin Upgrade"
+git commit -m "Deploying v1.9.2 - Hard Cache Reset & SW v10"
 git branch -M main
 git push -u origin main
 
