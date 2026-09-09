cd46755ed2de718f089051c82b8b964363a447a9
TU_NOMBRE_DE_USUARIO_GITHUB <TU_CORREO_DE_GITHUB@ejemplo.com>
2026-09-09 11:15:50 -0300

    Update

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'centinela-v535.2.3-root';
+const CACHE_NAME = 'centinela-v535.3.0-root';
 const ASSETS = [
     './',
-    './index.html?v=535.2.3',
-    './app/styles.css?v=535.2.3',
+    './index.html?v=535.3.0',
+    './app/styles.css?v=535.3.0',
     './app/supabase-config.js?v=535.2.3',
     './app/firebase-config.js?v=535.2.3',
     './app/db-v211.js?v=535.2.3',
